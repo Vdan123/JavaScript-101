fe26cf1e0a58d9f7d3b1ed277ad6cf1419c54a0e
V蛋 <610550052@qq.com>
2022-09-22 20:25:51 +0800

    init project

---
+++ 01-JS Drum Kit/test.js
@@ -0,0 +1,3 @@
+Array.from('foobar', (el) => {
+  console.log(el, 'eee')
+})
---
+++ app.spec.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'vitest'
+import { increment } from './app'
+
+describe('increment', () => {
+  test('increments the integer by 1', () => {
+    expect(increment(0, 10)).toBe(1)
+  })
+  test('does not increment the provided integer over the max', () => {
+    expect(increment(10, 10)).toBe(10)
+  })
+  test('default max number is 10', () => {
+    expect(increment(9)).toBe(10)
+  })
+})
+
